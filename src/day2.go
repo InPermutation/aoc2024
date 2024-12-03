faf51b7f6a9ace9636f26239c288461f2239c9ff
Jacob Krall <jacob@jkrall.net>
2024-12-02 22:54:00 -0500

    go: day 2: extract func isSafe

--- src/day2.go
+++ src/day2.go
@@ -46,30 +46,8 @@ func main() {
 		// Part 1:
 		safes := 0
 		for _, report := range reports {
-			asc, desc := true, true
-			last := report[0]
-			for i, v := range report {
-				if i == 0 {
-					continue
-				}
-				if last <= v {
-					desc = false
-				}
-				if last >= v {
-					asc = false
-				}
-				diff := last - v
-				if diff < 0 {
-					diff = -diff
-				}
-				if diff > 3 {
-					asc = false
-					desc = false
-				}
-
-				last = v
-			}
-			if asc || desc {
+			safe := isSafe(report)
+			if safe {
 				safes++
 			}
 		}
@@ -77,4 +55,31 @@ func main() {
 		fmt.Print("Safe: ")
 		fmt.Println(safes)
 	}
+}
+
+func isSafe(report []int) bool {
+	asc, desc := true, true
+	last := report[0]
+	for i, v := range report {
+		if i == 0 {
+			continue
+		}
+		if last <= v {
+			desc = false
+		}
+		if last >= v {
+			asc = false
+		}
+		diff := last - v
+		if diff < 0 {
+			diff = -diff
+		}
+		if diff > 3 {
+			asc = false
+			desc = false
+		}
+
+		last = v
+	}
+	return asc || desc
 }
