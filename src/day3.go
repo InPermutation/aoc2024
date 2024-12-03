f355ed9cc276355f393de235e8195bbdf01514dd
Jacob Krall <jacob@jkrall.net>
2024-12-03 00:15:12 -0500

    go: Day 3 Part 2 (regexp was a good idea)

--- src/day3.go
+++ src/day3.go
@@ -10,10 +10,11 @@ import (
 )
 
 func main() {
-	re := regexp.MustCompile("mul\\([0-9]{1,3},[0-9]{1,3}\\)")
+	re := regexp.MustCompile("do\\(\\)|don't\\(\\)|mul\\([0-9]{1,3},[0-9]{1,3}\\)")
 
 	for _, fname := range []string{
 		"input/03/sample",
+		"input/03/sample2",
 		"input/03/input",
 	} {
 		fmt.Println(fname)
@@ -27,8 +28,17 @@ func main() {
 
 		matches := re.FindAllString(str, -1)
 
-		var c int
+		var c, c2 int
+		enabled := true
 		for _, match := range matches {
+			if match == "do()" {
+				enabled = true
+				continue
+			}
+			if match == "don't()" {
+				enabled = false
+				continue
+			}
 			match = match[4:]
 			match = match[:len(match)-1]
 			args := strings.Split(match, ",")
@@ -44,10 +54,17 @@ func main() {
 				log.Fatal(err)
 			}
 			c += l * r
+			if enabled {
+				c2 += l * r
+			}
 		}
 
 		// Part 1:
 		fmt.Print("sum: ")
 		fmt.Println(c)
+
+		// Part 2:
+		fmt.Print("sum (conditionals): ")
+		fmt.Println(c2)
 	}
 }
