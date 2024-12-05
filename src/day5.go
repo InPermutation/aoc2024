85d742e6386b0ae9d8c3986ef4d820438bbdb9c2
Jacob Krall <jacob.krall@mongodb.com>
2024-12-05 16:30:17 -0500

    go: Day 5 Part 2

--- src/day5.go
+++ src/day5.go
@@ -48,6 +48,7 @@ func main() {
 		}
 
 		middlePageNumbersOk := 0
+		middlePageNumbersReordered := 0
 		for scanner.Scan() {
 			tokens := strings.Split(scanner.Text(), ",")
 
@@ -71,6 +72,63 @@ func main() {
 					log.Fatal(err)
 				}
 				middlePageNumbersOk += middlePageNumber
+			} else {
+				ordering := []page{}
+				pages := set{}
+				inserted := set{}
+
+				for _, v := range tokens {
+					pages[page(v)] = true
+				}
+
+				for len(pages) > 0 {
+					// find the lowest allowable page number
+					var lowest = page("INVALID")
+					for k := range pages {
+						isLowest := true
+						for k2, v := range orderingRules {
+							if k2 == k {
+								// ignore yourself
+								continue
+							}
+							if !pages[page(k2)] {
+								// this rule doesn't apply
+								continue
+							}
+							if inserted[page(k2)] {
+								// this page was already inserted
+								continue
+							}
+							if v[page(k)] {
+								// breaks rule
+								isLowest = false
+								break
+							}
+						}
+						if isLowest {
+							lowest = k
+							break
+						}
+					}
+
+					if lowest == page("INVALID") {
+						log.Fatal("couldn't find lowest")
+					}
+
+					// insert it into ordering
+					ordering = append(ordering, lowest)
+					inserted[lowest] = true
+
+					// remove it from pages
+					delete(pages, lowest)
+				}
+
+				middlePageNumber, err := strconv.Atoi(string(ordering[len(ordering)/2]))
+				if err != nil {
+					log.Fatal(err)
+				}
+				middlePageNumbersReordered += middlePageNumber
+
 			}
 		}
 		if err := scanner.Err(); err != nil {
@@ -80,5 +138,9 @@ func main() {
 		// Part 1:
 		fmt.Print("Sum of middle page numbers of correctly-ordered updates: ")
 		fmt.Println(middlePageNumbersOk)
+
+		// Part 2:
+		fmt.Print("Sum of middle page numbers of reordered updates: ")
+		fmt.Println(middlePageNumbersReordered)
 	}
 }
