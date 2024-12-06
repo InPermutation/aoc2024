a382985847d056db1bb7dd24e16648d8dd6547cc
Jacob Krall <jacob.krall@mongodb.com>
2024-12-06 13:13:45 -0500

    go: Day 6: extract state and methods

--- src/day6.go
+++ src/day6.go
@@ -7,109 +7,141 @@ import (
 	"os"
 )
 
-type page string
-type set map[page]bool
+type direction rune
+type state struct {
+	x     int
+	y     int
+	dir   direction
+	board [][]rune
+
+	distinct int
+}
+
+func NewStateFromFile(fname string) (rval *state) {
+	fmt.Println("NewStateFromFile", fname)
+
+	file, err := os.Open(fname)
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	rval = &state{
+		board: [][]rune{},
+	}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		bytes := make([]rune, len(line))
+
+		for i, b := range line {
+			bytes[i] = b
+			if b == '^' {
+				rval.x = i
+				rval.y = len(rval.board)
+				rval.dir = direction(b)
+			}
+		}
+
+		rval.board = append(rval.board, bytes)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return
+}
+func (p *state) something_in_front() bool {
+	switch p.dir {
+	case '^':
+		return p.y > 0 && p.board[p.y-1][p.x] == '#'
+	case '<':
+		return p.x > 0 && p.board[p.y][p.x-1] == '#'
+	case 'v':
+		return p.y+1 < len(p.board) && p.board[p.y+1][p.x] == '#'
+	case '>':
+		return p.x+1 < len(p.board[p.y]) && p.board[p.y][p.x+1] == '#'
+	default:
+		fmt.Println("err", p.dir)
+		panic(*p)
+	}
+}
+func (p *state) turn90() {
+	switch p.dir {
+	case '^':
+		p.dir = '>'
+	case '>':
+		p.dir = 'v'
+	case 'v':
+		p.dir = '<'
+	case '<':
+		p.dir = '^'
+	default:
+		panic(*p)
+	}
+}
+func (p *state) step() bool {
+	if p.board[p.y][p.x] != 'X' {
+		p.distinct++
+	}
+	p.board[p.y][p.x] = 'X'
+	switch p.dir {
+	case '^':
+		p.y--
+		return p.y >= 0
+	case '>':
+		p.x++
+		return p.x < len(p.board[p.y])
+	case 'v':
+		p.y++
+		return p.y < len(p.board)
+	case '<':
+		p.x--
+		return p.x >= 0
+	default:
+		panic(*p)
+	}
+}
+func (p *state) DeepCopy() (rval *state) {
+	rval = &state{
+		x:     p.x,
+		y:     p.y,
+		dir:   p.dir,
+		board: make([][]rune, len(p.board)),
+	}
+
+	for y, row0 := range p.board {
+		row := make([]rune, len(row0))
+
+		for x, o := range row0 {
+			row[x] = o
+		}
+
+		rval.board[y] = row
+	}
+
+	return rval
+}
 
 func main() {
 	for _, fname := range []string{
 		"input/06/sample",
 		"input/06/input",
 	} {
-		fmt.Println(fname)
-
-		file, err := os.Open(fname)
-		if err != nil {
-			log.Print(err)
+		state0 := NewStateFromFile(fname)
+		if state0 == nil {
 			continue
 		}
-		defer file.Close()
-		scanner := bufio.NewScanner(file)
 
-		var x0, y0 int
-		var dir0 rune
-
-		board0 := [][]rune{}
-
-		for scanner.Scan() {
-			line := scanner.Text()
-			bytes := make([]rune, len(line))
-
-			for i, b := range line {
-				bytes[i] = b
-				if b == '^' {
-					dir0 = b
-					x0 = i
-					y0 = len(board0)
-				}
-			}
-
-			board0 = append(board0, bytes)
-		}
-		if err := scanner.Err(); err != nil {
-			log.Fatal(err)
-		}
-
-		x, y, dir := x0, y0, dir0
-		board := deepcopy(board0)
-		fmt.Println(x, y, dir)
-
-		distinct := 0
-		something_in_front := func() bool {
-			switch dir {
-			case '^':
-				return y > 0 && board[y-1][x] == '#'
-			case '<':
-				return x > 0 && board[y][x-1] == '#'
-			case 'v':
-				return y+1 < len(board) && board[y+1][x] == '#'
-			case '>':
-				return x+1 < len(board[y]) && board[y][x+1] == '#'
-			default:
-				panic(dir)
-			}
-		}
-		turn90 := func() {
-			switch dir {
-			case '^':
-				dir = '>'
-			case '>':
-				dir = 'v'
-			case 'v':
-				dir = '<'
-			case '<':
-				dir = '^'
-			default:
-				panic(dir)
-			}
-		}
-		step := func() bool {
-			if board[y][x] != 'X' {
-				distinct++
-			}
-			board[y][x] = 'X'
-			switch dir {
-			case '^':
-				y--
-				return y >= 0
-			case '>':
-				x++
-				return x < len(board[y])
-			case 'v':
-				y++
-				return y < len(board)
-			case '<':
-				x--
-				return x >= 0
-			default:
-				panic(dir)
-			}
-		}
+		state := state0.DeepCopy()
 
 		for true {
-			if something_in_front() {
-				turn90()
+			if state.something_in_front() {
+				state.turn90()
 			} else {
-				if !step() {
+				if !state.step() {
 					break
 				}
 			}
@@ -117,21 +149,6 @@ func main() {
 
 		// Part 1:
 		fmt.Println("distinct positions: ")
-		fmt.Println(distinct)
+		fmt.Println(state.distinct)
 	}
-}
-
-func deepcopy(board0 [][]rune) [][]rune {
-	rval := make([][]rune, len(board0))
-	for y, row0 := range board0 {
-		row := make([]rune, len(row0))
-
-		for x, o := range row0 {
-			row[x] = o
-		}
-
-		rval[y] = row
-	}
-
-	return rval
 }
