98fc765f1fd171c05ec84c96bf282a3c5e71620e
Jacob Krall <jacob@jkrall.net>
2024-12-08 11:24:07 -0500

    go: Day 8 Part 1

---
+++ src/day8.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+type freq rune
+
+func (f freq) String() string {
+	return "'" + string(f) + "'"
+}
+
+type pos struct {
+	x int
+	y int
+}
+
+func (p pos) String() string {
+	return fmt.Sprintf("(%d, %d)", p.x, p.y)
+}
+
+func main() {
+	for _, fname := range []string{
+		"input/08/sample",
+		"input/08/input",
+	} {
+		fmt.Println(fname)
+		file, err := os.Open(fname)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+
+		y := 0
+		nodes := map[freq][]pos{}
+		var height, width int
+		for scanner.Scan() {
+			line := scanner.Text()
+			width = len(line)
+			for x, f := range line {
+				if f != '.' {
+					nodes[freq(f)] = append(nodes[freq(f)], pos{x, y})
+				}
+			}
+
+			y++
+			// establish bounds:
+			height, width = y, len(line)
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		antinodes := map[pos]bool{}
+		for _, ps := range nodes {
+			for i, p := range ps {
+				for _, p2 := range ps[i+1:] {
+					for _, cand := range resonate(p, p2) {
+						if cand.x < 0 || cand.y < 0 {
+							continue
+						}
+						if cand.x >= width || cand.y >= height {
+							continue
+						}
+						antinodes[cand] = true
+					}
+				}
+			}
+		}
+
+		// Part 1
+		fmt.Println("total unique antinode locations: ", len(antinodes))
+	}
+}
+func resonate(a, b pos) (rval []pos) {
+	dx := a.x - b.x
+	dy := a.y - b.y
+	return []pos{
+		pos{b.x - dx, b.y - dy},
+		pos{a.x + dx, a.y + dy},
+	}
+}
