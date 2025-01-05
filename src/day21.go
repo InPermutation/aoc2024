3074239ca58a61553816e37ce72246099e717013
Jacob Krall <jacob@jkrall.net>
2025-01-04 22:12:27 -0500

    go: day 21: incomplete

--- src/day21.go
+++ src/day21.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 type state struct {
@@ -54,6 +55,152 @@ func states() (rval []state) {
 func main() {
 	for _, s := range states() {
 		fmt.Println(s.fname)
-		fmt.Println(s)
+		sum := 0
+		for _, v := range s.codes {
+			numericPart, err := strconv.Atoi(v[:len(v)-1])
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(v, "(", len(v), ")")
+			d := driveNumeric(v)
+			fmt.Println(d, "(", len(d), ")")
+			d = driveDirectional(d)
+			fmt.Println(d, "(", len(d), ")")
+			d = driveDirectional(d)
+			fmt.Println(d, "(", len(d), ")")
+			sum += len(d) * numericPart
+			fmt.Println()
+		}
+		fmt.Println()
+		fmt.Println("sum complexity", sum)
 	}
+}
+
+var dd map[string]string = map[string]string{
+	"A^": "<",
+	"A>": "v",
+	"Av": "v<",
+	"A<": "v<<",
+
+	"<A": ">>^",
+	"<^": ">^",
+	"<v": ">",
+	"<>": ">>",
+
+	"v^": "^",
+	"v>": ">",
+	"v<": "<",
+	"vA": ">^",
+
+	"^A": ">",
+	"^<": "v<",
+	"^v": "v",
+	"^>": "v>",
+
+	">A": "^",
+	">^": "^<",
+	">v": "<",
+	"><": "<<",
+
+	"AA": "",
+	"^^": "",
+	"vv": "",
+	"<<": "",
+	">>": "",
+}
+
+func driveDirectional(s string) string {
+	//  ^A
+	// <v>
+	pos := "A"
+	rv := ""
+	for _, v := range s {
+		if i, ok := dd[pos+string(v)]; !ok {
+			log.Fatal("dd fail ", s, ":", pos, string(v))
+		} else {
+			rv += i + "A"
+			pos = string(v)
+		}
+	}
+
+	return rv
+}
+
+var nd map[string]string = map[string]string{
+	"A0": "<",
+	"A1": "^<<",
+	"A2": "^<",
+	"A3": "^",
+	"A4": "^^<<",
+	"A5": "^^<",
+	"A6": "^^",
+	"A7": "^^^<<",
+	"A8": "^^^<",
+	"A9": "^^^",
+
+	"0A": ">",
+	"01": "^<",
+	"02": "^",
+	"03": "^>",
+	"04": "^^<",
+	"05": "^^",
+	"06": "^^>",
+	"07": "^^^<",
+	"08": "^^^",
+	"09": "^^^>",
+
+	"17": "^^",
+
+	"2A": "v>",
+	"29": ">^^",
+
+	"3A": "v",
+	"37": "^^<<",
+	"38": "^^<",
+	"39": "^^",
+
+	"45": ">",
+	"46": ">>",
+
+	"56": ">",
+
+	"6A": "vv",
+	"63": "v",
+
+	"74": "v",
+	"75": "v>",
+	"76": "v>>",
+	"78": ">",
+	"79": ">>",
+
+	"80": "vvv",
+	"81": "vv<",
+	"82": "vv",
+	"83": "vv>",
+	"87": "<",
+	"89": ">",
+
+	"9A": "vvv",
+	"96": "v",
+	"97": "<<",
+	"98": "<",
+}
+
+func driveNumeric(s string) string {
+	// 789
+	// 456
+	// 123
+	//  0A
+	pos := "A"
+	rv := ""
+	for _, c := range s {
+		if s, ok := nd[pos+string(c)]; ok {
+			rv += s + "A"
+			pos = string(c)
+		} else {
+			log.Fatal(pos, string(c), ":", s)
+		}
+	}
+
+	return rv
 }
