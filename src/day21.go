00b7dd627093e48e0148b2ba8dd714ff3afbba8e
Jacob Krall <jacob@jkrall.net>
2025-01-06 13:23:11 -0500

    go: Day 21: Part 2

--- src/day21.go
+++ src/day21.go
@@ -56,19 +56,19 @@ func states() (rval []state) {
 func main() {
 	for _, s := range states() {
 		fmt.Println(s.fname)
-		sum := 0
+		sum, sum2 := 0, 0
 		for _, v := range s.codes {
 			numericPart, err := strconv.Atoi(v[:len(v)-1])
 			if err != nil {
 				log.Fatal(err)
 			}
-			d := driveNumeric(v)
-			d = driveDirectional(d)
-			d = driveDirectional(d)
-			sum += len(d) * numericPart
+			n := driveNumeric(v)
+			sum += lenDirectional(n, 2) * numericPart
+			sum2 += lenDirectional(n, 25) * numericPart
 		}
 		fmt.Println()
 		fmt.Println("sum complexity", sum)
+		fmt.Println("sum complexity 2", sum2)
 	}
 }
 
@@ -83,8 +83,34 @@ var directional map[rune]coord = map[rune]coord{
 	'>': coord{2, 1},
 }
 
-func driveDirectional(s string) string {
-	return drivePad(directional, s)
+type mstate struct {
+	next    rune
+	current rune
+	depth   int
+}
+
+var memo map[mstate]int = map[mstate]int{}
+
+func lenDirectional(d string, rounds int) (rv int) {
+	if rounds == 0 {
+		return len(d)
+	}
+	ms := mstate{
+		current: 'A',
+		depth:   rounds,
+	}
+	for _, ms.next = range d {
+		if v, ok := memo[ms]; ok {
+			rv += v
+		} else {
+			dd := drivePad(directional, ms.next, ms.current)
+			ld := lenDirectional(dd, rounds-1)
+			memo[ms] = ld
+			rv += ld
+		}
+		ms.current = ms.next
+	}
+	return
 }
 
 type coord struct {
@@ -113,19 +139,24 @@ var numpad map[rune]coord = map[rune]coord{
 }
 
 func driveNumeric(s string) string {
-	return drivePad(numpad, s)
+	from := 'A'
+	sb := strings.Builder{}
+	for _, to := range s {
+		sb.WriteString(drivePad(numpad, to, from))
+		from = to
+	}
+
+	return sb.String()
 }
 
-func drivePad(pad map[rune]coord, s string) string {
+func drivePad(pad map[rune]coord, to, from rune) string {
 	_, isDirectional := pad['<']
-	pos := pad['A']
+	pos := pad[from]
+
 	sb := strings.Builder{}
 
-	for _, c := range s {
-		next, ok := pad[c]
-		if !ok {
-			log.Fatal("unknown digit ", string(c), " in ", s)
-		}
+	{
+		next := pad[to]
 		diff := coord{next.x - pos.x, next.y - pos.y}
 		if isDirectional {
 			// don't hover X
@@ -135,7 +166,7 @@ func drivePad(pad map[rune]coord, s string) string {
 					diff.y--
 				}
 			}
-			if pos.x == 1 && pos.y == 1 {
+			if pos.x == 0 && pos.y == 1 {
 				for diff.x > 0 {
 					sb.WriteRune('>')
 					diff.x--
@@ -158,24 +189,30 @@ func drivePad(pad map[rune]coord, s string) string {
 			}
 		}
 
+		// < is always farthest on the d-pad
 		for diff.x < 0 {
 			sb.WriteRune('<')
 			diff.x++
 		}
+
+		// v is second-farthest
 		for diff.y > 0 {
 			sb.WriteRune('v')
 			diff.y--
+		}
+
+		// The order of these two took a lot of experimentation to find:
+		for diff.y < 0 {
+			sb.WriteRune('^')
+			diff.y++
 		}
 		for diff.x > 0 {
 			sb.WriteRune('>')
 			diff.x--
 		}
-		for diff.y < 0 {
-			sb.WriteRune('^')
-			diff.y++
-		}
+		// (I'm honestly still not 100% sure why the order matters.)
+
 		sb.WriteRune('A')
-		pos = next
 	}
 
 	return sb.String()
